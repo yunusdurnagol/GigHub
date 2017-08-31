7c283e206ff153cbaab4b589eec053b56d94213a
yunus <yunusdurnagol@gmail.com>
2017-08-31 20:43:25 +0300

    Implemen Underscore.js

--- GigHub/App_Start/BundleConfig.cs
+++ GigHub/App_Start/BundleConfig.cs
@@ -9,6 +9,7 @@ namespace GigHub
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                         "~/Scripts/underscore-min.js",
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(

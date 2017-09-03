6033332afecb5158ec50633e48058c181a7e4142
yunus <yunusdurnagol@gmail.com>
2017-09-03 18:53:40 +0300

    GigDetails Implementation

---
+++ GigHub/ViewModels/GigDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GigHub.Models;
+
+namespace GigHub.ViewModels
+{
+    public class GigDetailsViewModel
+    {
+        public Gig Gig { get; set; }
+        public bool IsFollowing { get; set; }
+        public bool IsAttending { get; set; }
+    }
+}
